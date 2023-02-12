14d1d080653f037d21ac65245e83cc7ab0521bba
atbranco <ateresabranco@gmail.com>
2023-02-12 12:32:07 +0100

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "aa1aa42435abecd0fo10t3c17ebc9f2a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
